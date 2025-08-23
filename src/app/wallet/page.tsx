7db00e5870078a0ea3b1c501464b647cc069b077
Hoang Lee <hoanglee26222@gmail.com>
2025-08-23 16:40:06 +0700

    fix plahodler

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1233,10 +1233,10 @@ export default function WalletPage() {
                                 </button>
                                 <button
                                     onClick={() => setShowImportWallets(true)}
-                                    className="lg:max-w-auto group relative bg-gradient-to-t from-[#377bf8] to-theme-secondary-400 py-1.5 px-3 md:pl-3 pr-4 rounded-full text-[10px] md:text-xs transition-all duration-500 hover:from-theme-blue-100 hover:to-theme-blue-200 hover:scale-105 hover:shadow-lg hover:shadow-theme-primary-500/30 active:scale-95 w-full md:w-auto flex items-center justify-center gap-1"
+                                    className="lg:max-w-auto group relative bg-transparent border py-1.5 px-4 md:pl-3 pr-4 rounded-full text-[10px] md:text-xs transition-all duration-500 hover:from-theme-blue-100 hover:bg-gradient-to-t hover:border-transparent hover:to-theme-blue-200 hover:scale-105 hover:shadow-lg hover:shadow-theme-primary-500/30 active:scale-95 w-full md:w-auto flex items-center justify-center gap-1 lg:bg-white dark:lg:bg-transparent  lg:border-transparent dark:lg:border-theme-neutral-100"
                                 >
                                     <div className="w-2.5 h-2.5 sm:w-3 sm:h-3 relative overflow-hidden text-theme-neutral-100">
-                                        <PlusIcon className="w-2.5 h-2.5 sm:w-3 sm:h-3" />
+                                        <ArrowDownToLine className="w-3.5 h-3.5 sm:w-4 sm:h-4" />
                                     </div>
                                     <div className="text-xs sm:text-sm font-medium capitalize leading-tight text-theme-neutral-100">
                                         {t('wallet.importWallets')}
@@ -1255,7 +1255,7 @@ export default function WalletPage() {
                                 </button>
                                 <button
                                     onClick={() => setShowImportWallet(true)}
-                                    className="lg:max-w-auto  group relative bg-transparent border py-1.5  px-3 md:pl-3 pr-4 rounded-full text-[10px] md:text-xs transition-all duration-500 hover:from-theme-blue-100 hover:bg-gradient-to-t hover:border-transparent hover:to-theme-blue-200 hover:scale-105 hover:shadow-lg hover:shadow-theme-primary-500/30 active:scale-95 w-full md:w-auto flex items-center justify-center gap-1 lg:bg-white dark:lg:bg-transparent  lg:border-transparent dark:lg:border-theme-neutral-100"
+                                    className="lg:max-w-auto group relative bg-transparent border py-1.5  px-3 md:pl-3 pr-4 rounded-full text-[10px] md:text-xs transition-all duration-500 hover:from-theme-blue-100 hover:bg-gradient-to-t hover:border-transparent hover:to-theme-blue-200 hover:scale-105 hover:shadow-lg hover:shadow-theme-primary-500/30 active:scale-95 w-full md:w-auto flex items-center justify-center gap-1 lg:bg-white dark:lg:bg-transparent  lg:border-transparent dark:lg:border-theme-neutral-100"
                                 >
                                     <ArrowDownToLine className="w-3.5 h-3.5 sm:w-4 sm:h-4" />
                                     <div className="text-xs sm:text-sm font-medium leading-tight text-indigo-500 dark:text-white">
@@ -1592,19 +1592,19 @@ export default function WalletPage() {
                                         />
                                     </div>
                                 </div>
-                                 {/* Network Selection */}
-                                 <div className="flex flex-col gap-1">
-                                        <div className={modalLabelStyles}>{t('wallet.country')}</div>
-                                        <div className={wrapGradientStyle}>
-                                            <CustomSelect
-                                                value={selectedNetwork}
-                                                onChange={(value) => setSelectedNetwork(value)}
-                                                options={langConfig.listLangs}
-                                                placeholder={t('wallet.country')}
-                                                t={t}
-                                            />
-                                        </div>
+                                {/* Network Selection */}
+                                <div className="flex flex-col gap-1">
+                                    <div className={modalLabelStyles}>{t('wallet.country')}</div>
+                                    <div className={wrapGradientStyle}>
+                                        <CustomSelect
+                                            value={selectedNetwork}
+                                            onChange={(value) => setSelectedNetwork(value)}
+                                            options={langConfig.listLangs}
+                                            placeholder={t('wallet.country')}
+                                            t={t}
+                                        />
                                     </div>
+                                </div>
                                 <div>
                                     <label className="block text-sm font-medium dark:text-gray-200 text-black mb-1">{t('wallet.connectMaster')}</label>
                                     <div className={wrapGradientStyle}>
@@ -1614,8 +1614,8 @@ export default function WalletPage() {
                                                 setSelectedMasterTrader(value);
                                             }}
                                         >
-                                            <SelectTrigger className="w-full px-3 py-1.5 dark:bg-theme-black-200 placeholder:text-sm rounded-xl text-black dark:text-theme-neutral-100 focus:outline-none focus:border-purple-500">
-                                                <SelectValue placeholder={t('wallet.connectMaster')} />
+                                            <SelectTrigger className="border-none w-full px-3 py-1.5 dark:bg-theme-black-200 bg-white placeholder:!text-xs rounded-xl text-black dark:text-theme-neutral-100 focus:outline-none focus:border-purple-500 [&>span]:text-xs dark:[&>span]:text-gray-400">
+                                                <SelectValue placeholder={t('wallet.connectMaster')} className="text-yellow-500" />
                                             </SelectTrigger>
                                             <SelectContent
                                                 className="bg-white dark:bg-theme-black-200 border border-gray-200 dark:border-gray-700 shadow-lg"
@@ -2065,7 +2065,7 @@ export default function WalletPage() {
                                     </button>
                                     <button
                                         onClick={handleImportWallet}
-                                        disabled={isLoading || !walletName || !walletNickname }
+                                        disabled={isLoading || !walletName || !walletNickname}
                                         className={modalButtonStyles}
                                     >
                                         <div className={modalButtonTextStyles}>{t('wallet.importWallet')}</div>
